90701a043dee4934f79fc294a10fce4fe857b9fd
Chukwurah Paul <paulchukwurah7@gmail.com>
2020-11-19 10:36:43 +0100

    fixed typos

--- src/Components/users/User.js
+++ src/Components/users/User.js
@@ -76,10 +76,10 @@ export class User extends Component {
                </div>
             </div>
             <div className="card card-badge text-center">
-               <div className="badge badge-primary">followers: {followers}</div>
-               <div className="badge badge-success">following: {following}</div>
-               <div className="badge badge-danger">public Repos: {public_repos}</div>
-               <div className="badge badge-dark">public Gists: {public_gists}</div>
+               <div className="badge badge-primary">Followers: {followers}</div>
+               <div className="badge badge-success">Following: {following}</div>
+               <div className="badge badge-danger">Public Repos: {public_repos}</div>
+               <div className="badge badge-dark">Public Gists: {public_gists}</div>
             </div>
             <h2>Recent Repos</h2>
             <Repos repos={repos}/>
