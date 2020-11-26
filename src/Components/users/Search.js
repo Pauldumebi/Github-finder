7c6fa660867fae864ca9ebcb774bb3154a521fb4
Chukwurah Paul <paulchukwurah7@gmail.com>
2020-11-26 17:48:13 +0100

    refactored code to functioal components

--- src/Components/users/Search.js
+++ src/Components/users/Search.js
@@ -1,39 +1,36 @@
-import React, { Component } from 'react'
+import React, {useState} from 'react'
 import PropTypes from 'prop-types'
 
-export class Search extends Component {
-   state = {
-      text: ''
-   } 
-   static propTypes = {
-      searchUsers: PropTypes.func.isRequired,
-      clearUsers: PropTypes.func.isRequired,
-      setAlert: PropTypes.func.isRequired,
+const Search = (searchUsers, showClear, clearUsers, setAlert) => {
+   const [text, setText] = useState('')
+
+   const onChange = (e) => setText(e.target.value)
    }
-   onChange = (e) => {
-      this.setState({ [e.target.name]: e.target.value })
-   }
-   onSubmit = (e) => {
+   const onSubmit = (e) => {
       e.preventDefault()
-      if (this.state.text === '') {
-         this.props.setAlert('Please enter a User', 'light')
+      if (text === '') {
+         setAlert('Please enter a User', 'light')
       } else {
-         this.props.searchUsers(this.state.text)
-         this.setState({ text: ''})
+         searchUsers(text)
+         setText('')
       }
    }
   
-   render() {
-      return (
-         <div>
-            <form className="form" style={{marginBottom: '1.5rem'}} onSubmit={this.onSubmit}> 
-               <input type="text" name="text" placeholder="Search Users..." style={{margin: '1.5rem 0',padding: '.5rem 1.5rem',  display: 'block', width: '100%', fontSize: '16px', border: '.1px #c0c0c0 solid'}} value={this.state.text} onChange={this.onChange}/>
-               <input type="submit" value="Search" className="btn btn-dark btn-block" style={{fontSize: '16px'}}/>
-            </form>
-            <button className="btn btn-light btn-block Clearbtn" onClick={this.props.clearUsers} style={{fontSize: '16px', marginBottom: '1.5rem', backgroundColor: '#0275d8'}}>Clear</button>
-         </div>
-      )
-   }
+   return (
+      <div>
+         <form className="form" style={{marginBottom: '1.5rem'}} onSubmit={onSubmit}> 
+            <input type="text" name="text" placeholder="Search Users..." style={{margin: '1.5rem 0',padding: '.5rem 1.5rem',  display: 'block', width: '100%', fontSize: '16px', border: '.1px #c0c0c0 solid'}} value={text} onChange={onChange}/>
+            <input type="submit" value="Search" className="btn btn-dark btn-block" style={{fontSize: '16px'}}/>
+         </form>
+         <button className= "btn btn-light btn-block Clearbtn" onClick={clearUsers} style={{fontSize: '16px', marginBottom: '1.5rem', backgroundColor: '#0275d8'}}>Clear</button>
+      </div>
+   )
+}
+ 
+static propTypes = {
+   searchUsers: PropTypes.func.isRequired,
+   clearUsers: PropTypes.func.isRequired,
+   setAlert: PropTypes.func.isRequired,
 }
 
 export default Search
